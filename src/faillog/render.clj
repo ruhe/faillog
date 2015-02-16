7861ff4985457e21df26635d9dfc48ddf2c42bbb
Ruslan Kamaldinov <rkamaldinov@mirantis.com>
2015-02-17 01:50:51 +0300

    Added very basic html rendering

--- src/faillog/render.clj
+++ src/faillog/render.clj
@@ -1,5 +1,22 @@
 (ns faillog.render
-  (:require [selmer.parser :refer [render-file]]))
+  (:require [selmer.parser :refer [render-file]]
+            [faillog.jenkins :as jenkins]
+            [faillog.launchpad :as lp]))
 
-(println (render-file "index.html"
-                      {:header "Hello WOrld!"}))
+
+(def jenkins "http://jenkins-product.srt.mirantis.net:8080/")
+(def job "6.1.staging.centos.bvt_1")
+
+(defn attach-bug [build]
+  (if (:bug build)
+    (assoc build :bug (lp/get-bug (:bug build)))
+    build))
+
+(defn attach-bugs [builds]
+  (sort-by :number > (map attach-bug builds)))
+
+
+(defn render-bugs []
+  (render-file "index.html"
+               {:builds (attach-bugs
+                         (jenkins/get-failed-builds jenkins job))}))
